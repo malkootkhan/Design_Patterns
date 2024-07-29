64c28301480a0e8293c9098e4c8ea65a10b3b444
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-07-29 17:00:24 +0100

    added the delivery object; and you can notice each field has its own object and in the implementation, only objects interact one another

--- FactoryMethod.cpp
+++ FactoryMethod.cpp
@@ -26,11 +26,43 @@ public:
     }
 };
 
+
+
+class Delivery
+{
+    public:
+        virtual ~Delivery() {}
+        string CarriageType;
+        virtual void carriage() = 0;
+    
+};
+class Truck:public Delivery
+{
+    public:
+        void carriage() override
+        {
+            CarriageType = "Truck";
+            cout<<CarriageType<<" is used to deliver the product"<<endl;
+        }
+};
+
+class Ship:public Delivery
+{
+    public:
+        void carriage() override
+        {
+            CarriageType = "Ship";
+            cout<<CarriageType<<" is used to deliver the product"<<endl;
+        }
+};
+
+
 // Creator Class
 class Factory {
 public:
     virtual ~Factory() {}
     virtual Product* createProduct() = 0;
+    virtual Delivery* createTransport() = 0;
 };
 
 // Concrete Creator: Smartphone Creator
@@ -38,6 +70,9 @@ class SmartphoneFactory : public Factory {
 public:
     Product* createProduct() override {
         return new Smartphone();
+    }
+    Delivery* createTransport() override {
+      return new Truck();  
     }
 };
 
@@ -47,24 +82,37 @@ public:
     Product* createProduct() override {
         return new Computer();
     }
+    Delivery* createTransport() override {
+        return new Ship();
+    }
 };
+
+
 
 int main() {
     Factory* myFactory = nullptr;
     Product* myProduct = nullptr;
+    Delivery* myDelivery = nullptr;
 
     // Create a Smartphone
     myFactory = new SmartphoneFactory();
     myProduct = myFactory->createProduct();
+    myDelivery = myFactory->createTransport(); //creating an object that will perform the delivery meachanisms
     myProduct->productLookLike();
+    myDelivery->carriage();
     delete myProduct;
+    delete myDelivery;
     delete myFactory;
+    
 
     // Create a Computer
     myFactory = new ComputerFactory();
     myProduct = myFactory->createProduct();
+    myDelivery = myFactory->createTransport(); //creating an object that will perform the delivery meachanisms
     myProduct->productLookLike();
+    myDelivery->carriage();
     delete myProduct;
+    delete myDelivery;
     delete myFactory;
 
     return 0;
